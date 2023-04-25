90bbca36a805aa5d829e15f539b2f56b2d8e5c48
Cherrywood <58643112+Cherrywoood@users.noreply.github.com>
2023-04-26 00:39:01 +0300

    create project

---
+++ src/main/java/com/example/ecomapbackend/EcomapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecomapbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomapBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomapBackendApplication.class, args);
+    }
+
+}
