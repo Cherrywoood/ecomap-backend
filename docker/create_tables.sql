585fb637ea508fe5af42c39e40ffaa32fbad1187
Cherrywood <58643112+Cherrywoood@users.noreply.github.com>
2023-04-27 00:06:46 +0300

    feat: change db model + add tests data

--- docker/create_tables.sql
+++ docker/create_tables.sql
@@ -1,79 +1,62 @@
-CREATE TYPE enum_type_ecopoint AS ENUM ('Место переработки', 'Экомагазин', 'Экоорганизация');
-CREATE TYPE enum_type_event AS ENUM ('Лекция', 'Мастер-класс', 'Выставка', 'Экскурсия', 'Семинар', 'Своп-вечеринка',
-    'Волонтерство', 'Развлекательная встреча', 'Разное');
 CREATE TYPE enum_day_week AS ENUM ('Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс');
 
-
-CREATE TABLE IF NOT EXISTS type_product
+CREATE TABLE IF NOT EXISTS waste_type
 (
-    id      SMALLSERIAL PRIMARY KEY,
-    NAME VARCHAR(25) NOT NULL UNIQUE CHECK (NAME != '')
+    id   SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(25) NOT NULL UNIQUE CHECK (name != '')
 );
 
-
-CREATE TABLE IF NOT EXISTS type_waste
+CREATE TABLE IF NOT EXISTS ecopoint_type_name
 (
-    id      SMALLSERIAL PRIMARY KEY,
-    NAME VARCHAR(25) NOT NULL UNIQUE CHECK (NAME != '')
-);
-
-CREATE TABLE IF NOT EXISTS map_point
-(
-    ID SERIAL PRIMARY KEY,
-    address text NOT NULL CHECK (address != ''),
-    coordinates geometry UNIQUE NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS event
-(
-    id serial PRIMARY KEY,
-    id_point INT NOT NULL UNIQUE,
-    Name varchar(100) NOT NULL CHECK(name != ''),
-    photo text NOT NULL, -- путь к файлу
-    description text NOT NULL,
-    type enum_type_event NOT NULL,
-    url_source text,
-    url_registration text,
-    start_date DATE NOT NULL,
-    start_time TIME,
-    end_date DATE,
-    FOREIGN KEY(id_point) REFERENCES map_point (id) ON DELETE CASCADE ON UPDATE CASCADE
+    id   SMALLSERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL UNIQUE CHECK (name != '')
 );
 
 CREATE TABLE IF NOT EXISTS ecopoint
 (
-    id serial PRIMARY KEY,
-    id_point INT NOT NULL UNIQUE,
-    NAME VARCHAR(255) NOT NULL CHECK(name != ''),
-    photo text NOT NULL, -- путь к файлу
-    description text NOT NULL,
-    type enum_type_ecopoint NOT NULL,
-    url text,
+    id           SERIAL PRIMARY KEY,
+    address      TEXT                   NOT NULL CHECK (address != ''),
+    geometry     GEOMETRY(POINT) UNIQUE NOT NULL,
+    name         VARCHAR(255)           NOT NULL CHECK (name != ''),
+    photo        VARCHAR(50), -- путь к файлу
+    description  TEXT                   NOT NULL,
+    site         TEXT,
     phone_number VARCHAR(11),
-    email varchar(320),
-    around_the_clock BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY(id_point) REFERENCES map_point (id) ON DELETE CASCADE ON UPDATE CASCADE
+    email        VARCHAR(320),
+    convenience  BOOLEAN DEFAULT FALSE  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS opening_hours
 (
-    id_ecopoint INT PRIMARY KEY,
-    day enum_day_week NOT NULL,
-    open_time TIME NOT NULL,
-    close_time TIME NOT NULL,
-    FOREIGN KEY(id_ecopoint) REFERENCES ecopoint (id) ON DELETE CASCADE ON UPDATE CASCADE
+    id          SERIAL PRIMARY KEY,
+    ecopoint_id INT                   NOT NULL,
+    day         enum_day_week         NOT NULL,
+    open_time   TIME,
+    close_time  TIME,
+    is_day_off  BOOLEAN DEFAULT FALSE NOT NULL,
+    FOREIGN KEY (ecopoint_id) REFERENCES ecopoint (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS ecopoint_type_waste -- type место переработки
+CREATE TABLE IF NOT EXISTS ecopoint_waste_type -- type место переработки
 (
-    ecopoint_id SMALLINT REFERENCES ecopoint (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    type_waste_id SMALLINT REFERENCES type_waste (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (ecopoint_id, type_waste_id)
+    ecopoint_id   SMALLINT REFERENCES ecopoint (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    waste_type_id SMALLINT REFERENCES waste_type (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (ecopoint_id, waste_type_id)
 );
 
-CREATE TABLE IF NOT EXISTS ecopoint_type_product -- type эко магазин
+CREATE TABLE IF NOT EXISTS ecopoint_type_rel -- type место переработки
 (
-    ecopoint_id     INT REFERENCES ecopoint (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    type_product_id SMALLINT REFERENCES type_product (id) ON DELETE CASCADE ON UPDATE CASCADE,
-    PRIMARY KEY (ecopoint_id, type_product_id)
+    ecopoint_id           SMALLINT REFERENCES ecopoint (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    ecopoint_type_name_id SMALLINT REFERENCES ecopoint_type_name (id) ON DELETE CASCADE ON UPDATE CASCADE,
+    PRIMARY KEY (ecopoint_id, ecopoint_type_name_id)
 );
+
+CREATE TABLE IF NOT EXISTS map_provider
+(
+    id          SMALLSERIAL PRIMARY KEY,
+    name        VARCHAR(25)           NOT NULL UNIQUE CHECK (name != ''),
+    url         TEXT                  NOT NULL CHECK (url != ''),
+    attribution TEXT                  NOT NULL CHECK (attribution != ''),
+    auth_token  TEXT CHECK (auth_token != ''),
+    is_default  BOOLEAN DEFAULT FALSE NOT NULL
+)
